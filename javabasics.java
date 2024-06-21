fbbc0bbe07b94327703ba1e6d03e4ed0fa74d3f1
Raghav Naithani <raghavnaithani2006@gmail.com>
2024-06-21 18:16:01 +0530

    learnt basic concepts of java

---
+++ javabasics.java
@@ -0,0 +1,29 @@
+ 
+ import java.util.Scanner;
+
+public class javabasics {
+      
+      // // code/ syntax for taking an input from user
+      
+      // public static void main(String[] args) {
+      //       System.out.println("enter a number");
+      //       Scanner s= new Scanner(System.in);               //system in is to take input from keyboard
+      //       int number = s.nextInt();
+      //        System.out.println(number);
+      // }
+
+      // public static void main(String[] args) {
+      //       System.out.println("enter  a string");
+      //       Scanner s= new Scanner(System.in);
+      //       String str = s.next();      // next only takes input till a space               //use nextLine for getting a whole line as inputs
+      //       System.out.println(str);
+      // }
+
+
+
+
+      
+      }
+
+
+
